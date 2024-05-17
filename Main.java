b6774b62f6d4ab95e528814adb72f9ed01fca48a
Aniketh Sahu <anikethsahu580@gmail.com>
2024-05-17 16:39:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Lockers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			Application menu = new Application();
+			menu.introDisp();
+			menu.mainMenuOp();
+		
+	}
+}
